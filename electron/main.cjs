8782c3c7480188ccecb02b09fb0076fc32aafbe4
Marino Villar <marinovillarlara@gmail.com>
2025-01-19 22:57:56 -0400

    Adding logs and app info

--- electron/main.cjs
+++ electron/main.cjs
@@ -2,6 +2,7 @@ const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
 const { ipcMain } = require('electron');
 require('dotenv').config()
+const log = require('electron-log');
 
 let mainWindow;
 
@@ -28,12 +29,17 @@ const createWindow = () => {
     },
   });
 
-  //if (process.env.NODE_ENV === 'development') {
+  log.info('App is starting...');
+  log.info('App is starting...', process.env);
+
+  if (process.env.NODE_ENV === 'development') {
     console.log('Development')
     mainWindow.loadURL('http://localhost:5173');
-  //} else {
-    //mainWindow.loadURL(path.join(__dirname, '../dist/index.html'));
-  //}
+  } else {
+    log.info('Dirname', __dirname);
+    log.info('entry point: ', path.join(__dirname, '../dist/index.html'))
+    mainWindow.loadURL(path.join(__dirname, '../dist/index.html'));
+  }
 };
 
 app.whenReady().then(() => {
--- electron/preload.cjs
+++ electron/preload.cjs
@@ -2,5 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
   performAction: async (args) => ipcRenderer.invoke('perform-action', args),
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
   // Expose any necessary functions or objects
 });
